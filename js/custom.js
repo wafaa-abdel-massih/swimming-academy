fe2a0dc0bb6468f6636f8f2f71366f8a4776b610
wafaa-abdel-massih <wafaa2016170498@cis.asu.edu.eg>
2021-03-27 02:48:48 +0200

    added reviews slider

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,58 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 });
+
+// review section functions 
+
+const slides = document.getElementsByClassName("slider");
+const dots = document.getElementsByClassName("dot");
+
+var slideIndex = 2;
+showSlides(slideIndex);
+
+const previousButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+previousButton.addEventListener('click', () => {
+    plusSlide(-1)
+});
+
+nextButton.addEventListener('click', () => {
+    plusSlide(1)
+});
+
+Array.prototype.forEach.call(dots, function(el){
+    el.addEventListener('click', () =>{
+        currentSlide(parseInt(el.getAttribute('data-number')));
+    })
+});
+
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+
+function plusSlide(n){
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+
+    if(n <= 0){
+        slideIndex = slides.length;
+    }
+
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
